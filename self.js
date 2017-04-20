d068f01d170f6ca9ee0e42892d0ac40c820078a7
Agata Zarod <Agata Zarod>
2017-04-20 16:14:02 +0200

    poprawa cudzysłowia

--- self.js
+++ self.js
@@ -5,15 +5,15 @@ function Button(text) {
 Button.prototype = {
 	create: function() {
 	var self = this;
-	this.$element = $(‘<button>’);
+	this.$element = $('<button>');
 	this.$element.text(this.text);
 	this.$element.click(function() {
 		alert(self.text);
-	$(‘body’).append(this.$element);
+	$('body').append(this.$element);
 	});
 }
 
-	var btn1 = new Button(‘Hello!’);
+	var btn1 = new Button('Hello!');
 	btn1.create();
 }
 
